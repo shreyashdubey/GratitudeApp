e3cb535a0aee4319010acbb46c57cedc514d8f4e
DonCorleone <shreyash@conzumex.com>
2023-09-26 15:57:55 +0530

    Update CardViewModel.swift

--- Gratitude/ViewModel/CardViewModel.swift
+++ Gratitude/ViewModel/CardViewModel.swift
@@ -10,16 +10,11 @@ import SwiftUI
 class CardViewModel: ObservableObject {
     @Published var title: String
     @Published var isLoading: Bool
-    @Published var isPreviousAvailble: Bool = true
     @Published var image: UIImage
-    @Published var postURL: URL
-    private let calendar = Calendar.current
     
     init(title: String, isLoading: Bool, isPreviousAvailble: Bool, image: UIImage, postURL: URL) {
         self.title = title
         self.isLoading = isLoading
-        self.isPreviousAvailble = isPreviousAvailble
         self.image = image
-        self.postURL = postURL
     }
 }
